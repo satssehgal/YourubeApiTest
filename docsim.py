6108556b93f011e70e69564cc1bee9fc919722ce
Sahitya Sehgal <satssehgal@gmail.com>
2020-05-24 21:28:45 -0400

    t

--- docsim.py
+++ docsim.py
@@ -1,21 +1,16 @@
-from flask import Flask, request, render_template
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import nltk
-#nltk.download('stopwords')
-from nltk.corpus import stopwords 
 
-set(stopwords.words('english'))
+stopwords_ios= ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
-def YTCompare(searchterms, title):
-	stop_words = stopwords.words('english')
-	text1 = searchterms
-	text2 = title
-	processed_doc1=' '.join([word for word in text1.split() if word not in stop_words])
-	processed_doc2=' '.join([word2 for word2 in text2.split() if word2 not in stop_words])
+def YTCompare(search_term, title):
+	processed_doc1=' '.join([word for word in search_term.split() if word not in stopwords_ios])
+	processed_doc2=' '.join([word2 for word2 in title.split() if word2 not in stopwords_ios])
 	corpus = [processed_doc1, processed_doc2]
 	vectorizer = TfidfVectorizer()
 	tfidf = vectorizer.fit_transform(corpus)
 	similarity_matrix = cosine_similarity(tfidf)[0,1]
-	return similarity_matrix
+	return similarity_i
+
+
 
